28cde41fe76b509d492ff9fca8e0cffc4f22bcea
kirillonishuk <Kiryl_Anishchuk@epam.com>
2018-07-10 12:01:58 +0300

    add .eslintignore

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -5,12 +5,12 @@ var baseConfig = require('./webpack.base.config');
 var optimizationConfig = require('./webpack.opt.config');
 
 const productionConfiguration = function (env) {
-  const NODE_ENV = env.NODE_ENV ? env.NODE_ENV : 'development';
-  return {
-    plugins: [
-      new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(NODE_ENV) }),
-    ]
-  };
+    const NODE_ENV = env.NODE_ENV ? env.NODE_ENV : 'development';
+    return {
+        plugins: [
+            new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(NODE_ENV) }),
+        ]
+    };
 }
 
 module.exports = merge.smart(baseConfig, optimizationConfig, productionConfiguration);
